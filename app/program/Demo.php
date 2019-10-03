f13e5800f1c40fdd55995f601331c83d53150665
xiaozhuox <mrxzx@wwsg18.top>
2019-10-04 00:09:26 +0800

    更新目录结构

--- app/program/Demo.php
+++ app/program/Demo.php
@@ -2,17 +2,20 @@
 
 namespace APP\program;
 
-class Demo{
-    public function Add($num1,$num2){
+class Demo
+{
+    public function Add($num1, $num2)
+    {
         return $num1 + $num2;
     }
 
-    public function Reduce($num1,$num2){
+    public function Reduce($num1, $num2)
+    {
         return $num1 - $num2;
     }
 
-    public function Test(){
+    public function Test()
+    {
         return "This is Test Funciton in Demo";
     }
-
 }
